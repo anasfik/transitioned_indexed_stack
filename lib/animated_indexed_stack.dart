d5fd0ab8b99237b7741446ea9c461630d05fb1f0
Gwhyyy <ffikhi.aanas@gmail.com>
2022-12-20 23:12:22 +0100

    [ Add ]added slide animated indexed stack

--- lib/animated_indexed_stack.dart
+++ lib/animated_indexed_stack.dart
@@ -1,4 +1,5 @@
 library animated_indexed_stack;
 
-export 'src/animated_fade_indexed_stack.dart';
-export 'src/animated_scale_indexed_stack.dart';
+export 'src/animated_fade_indexed_stack/animated_fade_indexed_stack.dart';
+export 'src/animated_scale_indexed_stack/animated_scale_indexed_stack.dart';
+export 'src/animated_slide_indexed_stack/animated_slide_indexed_stack.dart';
